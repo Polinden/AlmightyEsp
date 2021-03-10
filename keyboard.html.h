aef435e26feeb4fdbb133ad99e7e8624b51ed5a3
Den Wolper <denwolper@MacBook-Pro-Den.local>
2021-03-10 13:52:42 +0200

    bagfix

--- keyboard.html.h
+++ keyboard.html.h
@@ -1,3 +1,9 @@
+#pragma once
+#include "pagehelper.h"    
+namespace keyboard_html
+{
+CreatePageName; 
+HTMLPAGE = R"===(
 <!DOCTYPE html>
 <html>
 <head>
@@ -74,3 +80,6 @@ function getRandomInt(max) {
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 </html>
+
+)===";
+}
